1a646028cd61e707b39db325f89d9fbfe1012926
HoangTienHoa <HoangTienHoa@users.noreply.github.com>
2017-06-19 18:55:42 +0700

    Create Test Project

---
+++ Test/src/Test1.java
@@ -0,0 +1,23 @@
+
+class HoaException extends IndexOutOfBoundsException{
+	
+}
+class Person{
+	public static void method1(Person a) throws IndexOutOfBoundsException{
+		System.out.println(a.toString());
+		throw new HoaException();
+	}
+}
+public class Test1 {
+
+	public static void main(String[] args) {
+		Person a = new Person();
+		System.out.println(a.toString());
+		try {
+			Person.method1(a);
+		} catch (Exception e) {
+		  System.out.println(e.getClass());
+		}
+	}
+
+}
